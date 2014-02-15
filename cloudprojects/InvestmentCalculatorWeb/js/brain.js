466c2231aab13ae93447ac002176ab660747deff
Edwin <mal.edwin@vinadinonline.com>
2014-02-15 15:40:23 +0530

    Cool Progress

--- cloudprojects/InvestmentCalculatorWeb/js/brain.js
+++ cloudprojects/InvestmentCalculatorWeb/js/brain.js
@@ -8,18 +8,22 @@ $(document).ready(function(){
     
 function startProcessing(){
     $('#add').click(function(){
-        alert($(this).parentsUntil("tr"));
-        alert($("#repeater").parentsUntil("table"));
+        //alert($(this).parentsUntil("tr"));
+        //alert($("#repeater").parentsUntil("table"));
        $("#workTable").append($("#repeater").clone());
        $("#workTable").last.children();
         
     });
     
-    /*$('#amount').change(function(){
+    $('#amount').change(function(){
         alert(this.value); 
-    }); */
-    
-    $('#year').change(function(){
-        
+        //alert($(this).closest("tr").find('#year').value); /*.find("#year").value);*/
+        $(this).closest("tr").find('#year').css({"color":"red","border":"2px solid red"});
+        var year = $(this).closest("tr").find('#year').val();
+        alert(year);
     });
+    
+    /*$('#year').change(function(){
+        
+    });*/
 }
