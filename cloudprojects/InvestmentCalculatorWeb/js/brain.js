370c308de8e616b85ef3e69da3d82ca0210c9a50
Edwin <mal.edwin@vinadinonline.com>
2014-02-15 14:56:51 +0530

    final working state

--- cloudprojects/InvestmentCalculatorWeb/js/brain.js
+++ cloudprojects/InvestmentCalculatorWeb/js/brain.js
@@ -2,4 +2,24 @@
  * 
  */
 
-$document();
+$(document).ready(function(){
+    startProcessing();
+    });
+    
+function startProcessing(){
+    $('#add').click(function(){
+        alert($(this).parentsUntil("tr"));
+        alert($("#repeater").parentsUntil("table"));
+       $("#workTable").append($("#repeater").clone());
+       $("#workTable").last.children();
+        
+    });
+    
+    /*$('#amount').change(function(){
+        alert(this.value); 
+    }); */
+    
+    $('#year').change(function(){
+        
+    });
+}
